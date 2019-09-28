8287b13c0052a10cb6b9793389d612e392a74817
racheldmiller <rachelmiller415@gmail.com>
2019-09-28 11:35:24 -0700

    first one

---
+++ Get the Middle Letter/task.js
@@ -0,0 +1,16 @@
+// -------------------------------- TASK -------------------------------------
+
+// You are going to be given a word. Your job is to return the middle character of the word.
+// If the word's length is odd, return the middle character.
+// If the word's length is even, return the middle 2 characters.
+
+// -------------------------------- EXAMPLE ----------------------------------
+
+// Kata.getMiddle("test") should return "es"
+// Kata.getMiddle("testing") should return "t"
+// Kata.getMiddle("middle") should return "dd"
+// Kata.getMiddle("A") should return "A"
+
+// ----------------------------- APPROACH ------------------------------------
+
+// 1. Create a
